361cde1f5d67c1144c776151e8f177b3834ef5ac
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2023-02-21 00:18:21 +0100

    Continue dialog, auto save position, ui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use gtk::{
-    glib::ExitCode, prelude::*, Application, Box, Button, Orientation, ScrolledWindow, Window,
+    glib::ExitCode, prelude::*, Box, Button, Dialog, Label, ListBox, Orientation, Picture,
+    ResponseType, ScrolledWindow, Window,
 };
+use libadwaita::Application;
 use regex::Regex;
 use std::{ffi::OsStr, process::Command};
 use walkdir::WalkDir;
@@ -14,27 +16,37 @@ fn main() -> ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let content = Box::new(Orientation::Vertical, 5);
+    let hbox = Box::new(Orientation::Horizontal, 0);
+    let list_box = ListBox::new();
+    hbox.set_halign(gtk::Align::End);
+    // hbox.set_valign(gtk::Align::Center);
 
-    content.set_margin_end(10);
-    content.set_margin_start(10);
-    content.set_margin_top(10);
-    content.set_margin_bottom(10);
+    // let pixbuf = Pixbuf::from_file("RyuuguuRena.png").unwrap();
+    let poster = Picture::new();
+    hbox.append(&poster);
+
+    list_box.set_margin_end(10);
+    list_box.set_margin_start(10);
+    list_box.set_margin_top(10);
+    list_box.set_margin_bottom(10);
 
     let scrolled_window = ScrolledWindow::builder()
-        .child(&content)
+        .child(&list_box)
         .has_frame(true)
         .build();
+    scrolled_window.set_hscrollbar_policy(gtk::PolicyType::Never);
+
+    hbox.append(&scrolled_window);
 
     scrolled_window.set_margin_end(10);
-    scrolled_window.set_margin_start(10);
     scrolled_window.set_margin_top(10);
     scrolled_window.set_margin_bottom(10);
+    scrolled_window.set_margin_start(10);
 
     let main_window = Window::builder()
         .application(app)
         .title("movies")
-        .child(&scrolled_window)
+        .child(&hbox)
         .build();
 
     for file in WalkDir::new("/home/nikola/Media/befafd9f-f32e-4121-978d-5abfe9b6bf6c/movies/")
@@ -42,23 +54,43 @@ fn build_ui(app: &Application) {
         .filter_map(|file| file.ok())
     {
         if file.path().extension() == Some(OsStr::new("mp4")) {
-            let movie_name: &str = file.file_name().to_str().unwrap();
+            let movie_name: String = file.file_name().to_str().unwrap().to_string();
 
-            let button = Button::builder().label(name_parse(movie_name)).build();
-            button.connect_clicked(move |_| {
-                Command::new("mpv")
-                    .arg(OsStr::new(file.path().to_str().unwrap()))
-                    .spawn()
-                    .expect("error");
+            let dialog = Dialog::builder().transient_for(&main_window).build();
+            dialog.add_buttons(&[("Yes", ResponseType::Accept), ("No", ResponseType::Reject)]);
+            dialog
+                .content_area()
+                .append(&Label::new(Some("Continue watching?")));
+            dialog.content_area().set_margin_end(10);
+            dialog.content_area().set_margin_start(10);
+            dialog.content_area().set_margin_top(10);
+            dialog.content_area().set_margin_bottom(10);
+            dialog.connect_response(move |dialog, resp| {
+                match resp {
+                    ResponseType::Accept => {
+                        play_movie(file.path().to_str().unwrap().to_string(), false)
+                    }
+                    ResponseType::Reject => {
+                        play_movie(file.path().to_str().unwrap().to_string(), true)
+                    }
+                    _ => {}
+                };
+                dialog.hide();
             });
-            content.append(&button);
+
+            let button = Button::builder().label(name_parse(&movie_name)).build();
+            button.connect_clicked(move |_| {
+                dialog.show();
+            });
+
+            list_box.append(&button);
+            button.parent().unwrap().set_focusable(false);
         }
     }
-    main_window.connect_notify::<_>(Some("default-width"), |window, _| {
-        window.set_default_width(window.width());
-        window.child().unwrap().set_margin_start(window.width() / 3);
-    });
-
+    // main_window.connect_notify::<_>(Some("default-width"), |window, _| {
+    //     window.set_default_width(window.width());
+    //     window.child().unwrap().set_margin_start(window.width() / 3);
+    // });
     main_window.present();
 }
 
@@ -67,7 +99,17 @@ fn name_parse(name: &str) -> String {
     let binding = re.captures(name);
     let name: String = match &binding {
         Some(expr) => format!("{} ({})", &expr[1], &expr[2]),
-        None => name.to_string(),
+        None => name.to_string().replace(".mp4", ""),
     };
     name.replace(".", " ")
+}
+
+fn play_movie(path: String, from_start: bool) {
+    Command::new("mpv")
+        .arg(OsStr::new(&path))
+        .arg("--save-position-on-quit")
+        // .arg("--write-filename-in-watch-later-config")
+        .arg(if from_start { "--start=0%" } else { "" })
+        .output()
+        .expect("Movie failed to play");
 }
