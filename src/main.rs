2bf78ae9882465ec35c8a25fbe9c22f0b9133fc6
ShiNoNeko47 <ShiNoNeko47@proton.me>
2023-07-15 13:12:35 +0200

    Replaced if else with match for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod app;
 mod main_window;
 
-use glib::{user_data_dir, user_cache_dir};
+use glib::{user_cache_dir, user_data_dir};
 use gtk::glib::ExitCode;
 use std::{env, process::exit};
 
@@ -10,11 +10,15 @@ fn main() -> ExitCode {
     if args.len() == 1 {
         let app = app::App::new();
         app.run()
-    } else if args.contains(&"--version".to_string()) || args.contains(&"-v".to_string()) {
-        println!("Version: {}", env!("CARGO_PKG_VERSION"));
-        exit(0)
-    } else if args.contains(&"--help".to_string()) || args.contains(&"-h".to_string()) {
-        println!("Usage: {} [option]
+    } else {
+        match args[1].as_str() {
+            "--version" | "-v" => {
+                println!("Version: {}", env!("CARGO_PKG_VERSION"));
+                exit(0)
+            }
+            "--help" | "-h" => {
+                println!(
+                    "Usage: {} [option]
 
   -v, --version \t show version and exit
   -h, --help \t\t show this help and exit
@@ -23,25 +27,37 @@ fn main() -> ExitCode {
 
 You can add to library from within the app, or you can create symlinks to
 directories with movies in \"{}/\"
-(e.g. ln -s FULL_PATH_TO_DIRECTORY {}/NAME", args[0], reel_hub::utils::user_dir(user_data_dir()), reel_hub::utils::user_dir(user_data_dir()));
-        exit(0)
-    } else if args.contains(&"--list".to_string()) || args.contains(&"-l".to_string()) {
-        let movies = reel_hub::detect::get_movies(reel_hub::utils::user_dir(user_data_dir()));
-        for movie in movies {
-            if let Some(year) = movie.year {
-                println!("{} ({})", movie.name, year);
-            } else {
-                println!("{}", movie.name);
+(e.g. ln -s FULL_PATH_TO_DIRECTORY {}/NAME",
+                    args[0],
+                    reel_hub::utils::user_dir(user_data_dir()),
+                    reel_hub::utils::user_dir(user_data_dir())
+                );
+                exit(0)
+            }
+            "--list" | "-l" => {
+                let movies =
+                    reel_hub::detect::get_movies(reel_hub::utils::user_dir(user_data_dir()));
+                for movie in movies {
+                    if let Some(year) = movie.year {
+                        println!("{} ({})", movie.name, year);
+                    } else {
+                        println!("{}", movie.name);
+                    }
+                }
+                exit(0)
+            }
+            "--clear-cache" | "-c" => {
+                if let Result::Ok(_) =
+                    std::fs::remove_dir_all(reel_hub::utils::user_dir(user_cache_dir()))
+                {
+                    println!("Cache cleared");
+                }
+                exit(0)
+            }
+            _ => {
+                println!("Unknown command line arguments");
+                exit(1)
             }
         }
-        exit(0)
-    } else if args.contains(&"--clear-cache".to_string()) || args.contains(&"-c".to_string()) {
-        if let Result::Ok(_) = std::fs::remove_dir_all(reel_hub::utils::user_dir(user_cache_dir())) {
-            println!("Cache cleared");
-        }
-        exit(0)
-    } else {
-        println!("Unknown command line arguments");
-        exit(1)
     }
 }
