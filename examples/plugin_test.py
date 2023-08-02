84115340f252d228c4392a70c0ebe2beb59b5055
ShiNoNeko47 <ShiNoNeko47@proton.me>
2023-08-02 19:26:24 +0200

    Updated plugin example

--- examples/plugin_test.py
+++ examples/plugin_test.py
@@ -4,4 +4,12 @@ while True:
     if s == "0":
         break
     elif s == "add":
-        print("movie;Ringu;1998;/home/nikola/.local/test/Ringu.1998.mp4")
+        print(
+            "movie;HRT1;;https://webtvstream.bhtelecom.ba/hls9/hrt1_1200.m3u8;HRT1;HRT1;Hrvatska radio televizija;hr;/hrt1.png;/hrt1.png;;;;tv"
+        )
+        print(
+            "movie;RTL;;https://d1cs5tlhj75jxe.cloudfront.net/rtl/playlist.m3u8;RTL;RTL"
+        )
+        print(
+            "movie;Foolproof;2003;https://www.youtube.com/watch?v=crXAhKcpjNc&pp=ygULZnV1bCBtb3ZpZXM%3D"
+        )
